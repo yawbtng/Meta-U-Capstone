1490d184ca9649559da39e0788ebe1cf746a8842
Yaw Boateng <yawbtng@meta.com>
2025-07-09 11:09:40 -0700

    Create edit contact sidebar and connect it to row actions

---
+++ frontend/src/components/edit-contact-sheet.jsx
@@ -0,0 +1,44 @@
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetDescription,
+  SheetFooter,
+  SheetHeader,
+  SheetTitle,
+} from "@/components/ui/sheet"
+
+export function EditContact({children}) {
+  return (
+    <Sheet>
+        {children}
+      <SheetContent>
+        <SheetHeader>
+          <SheetTitle>Edit Contact</SheetTitle>
+          <SheetDescription>
+            Make changes to this contact here. Click save when you are done.
+          </SheetDescription>
+        </SheetHeader>
+        <div className="grid flex-1 auto-rows-min gap-6 px-4">
+          <div className="grid gap-3">
+            <Label htmlFor="sheet-demo-name">Name</Label>
+            <Input id="sheet-demo-name" defaultValue="Yaw Boateng" />
+          </div>
+          <div className="grid gap-3">
+            <Label htmlFor="sheet-demo-username">Username</Label>
+            <Input id="sheet-demo-username" defaultValue="@yawbtng_" />
+          </div>
+        </div>
+        <SheetFooter>
+          <Button type="submit">Save changes</Button>
+          <SheetClose asChild>
+            <Button variant="outline">Close</Button>
+          </SheetClose>
+        </SheetFooter>
+      </SheetContent>
+    </Sheet>
+  )
+}
