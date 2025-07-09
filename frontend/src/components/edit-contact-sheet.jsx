552b40683f628b5c631f038fe9b0a4573249db81
Yaw Boateng <yawbtng@meta.com>
2025-07-09 11:43:21 -0700

    Create basic outline for edit contact sheet

--- frontend/src/components/edit-contact-sheet.jsx
+++ frontend/src/components/edit-contact-sheet.jsx
@@ -10,6 +10,26 @@ import {
   SheetHeader,
   SheetTitle,
 } from "@/components/ui/sheet"
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion"
+import AvatarDemo from "./avatar-01"
+
+const SectionBreakdown = ({children, title}) => {
+    return (
+        <Accordion type="single" collapsible>
+            <AccordionItem value="item-1">
+                <AccordionTrigger>{title}</AccordionTrigger>
+                <AccordionContent>
+                    {children}
+                </AccordionContent>
+            </AccordionItem>
+        </Accordion>
+    )
+}
 
 export function EditContact({children}) {
   return (
@@ -22,6 +42,9 @@ export function EditContact({children}) {
             Make changes to this contact here. Click save when you are done.
           </SheetDescription>
         </SheetHeader>
+
+        <AvatarDemo initials="YB" />
+
         <div className="grid flex-1 auto-rows-min gap-6 px-4">
           <div className="grid gap-3">
             <Label htmlFor="sheet-demo-name">Name</Label>
@@ -31,6 +54,31 @@ export function EditContact({children}) {
             <Label htmlFor="sheet-demo-username">Username</Label>
             <Input id="sheet-demo-username" defaultValue="@yawbtng_" />
           </div>
+
+          <div>
+        
+            
+            <SectionBreakdown title="Basic Information">
+
+            </SectionBreakdown>
+
+            <SectionBreakdown title="Professional Information">
+
+            </SectionBreakdown>
+
+            <SectionBreakdown title="Background & Context">
+
+            </SectionBreakdown>
+
+            <SectionBreakdown title="Relationship & Categories">
+
+            </SectionBreakdown>
+
+            <SectionBreakdown title="Social Media & Additional Notes">
+
+            </SectionBreakdown>
+          </div>
+
         </div>
         <SheetFooter>
           <Button type="submit">Save changes</Button>
