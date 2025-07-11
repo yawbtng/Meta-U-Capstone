85b2fa986ab83f08227716a9d1f8edf621744447
Yaw Boateng <yawbtng@meta.com>
2025-07-11 16:49:24 -0700

    Implement and test trie logic on frontend

--- frontend/src/components/search-contacts.jsx
+++ frontend/src/components/search-contacts.jsx
@@ -1,15 +1,40 @@
 import {useState, useEffect, useRef} from "react"
 import { Search } from "lucide-react";
+import { Trie } from "../../../backend/trie"
+import { fetchInitialContactsForSearch } from "../../../backend";
 
 const SearchContacts = () => {
     const [searchTerm, setSearchTerm] = useState("");
     const [results, setResults] = useState([]);
     const [showDropdown, setShowDropdown] = useState(false);
+    const [trie, setTrie] = useState(new Trie());
     const typeaheadRef = useRef(null)
+    
 
-    const handleChange = (e) => {
-        setSearchTerm(e.target.value)
+    const handleChange = async (e) => {
+        const input = e.target.value
+        setSearchTerm(input)
+
+        if (input.length === 0) {
+            setResults([])
+            setShowDropdown(false)
+            setTrie(new Trie())
+        }
+
+        if (input.length === 1) {
+            const contacts = await fetchInitialContactsForSearch(input)
+            const newTrie = new Trie();
+            newTrie.batchInsert(contacts)
+            setTrie(newTrie)
+            setResults(newTrie.findContactsWithPrefix(input))
+            setShowDropdown(true)
+        } else {
+            setResults(trie.findContactsWithPrefix(input))
+        }
+
     }
+
+
 
 
     useEffect(() => {
