d33c6d112789c63db0f914fe98c03a1b406e5ebd
yawbtng <154343001+yawbtng@users.noreply.github.com>
2025-07-11 21:54:51 -0700

    export all supabase calls to use on frontend

--- Lynk/backend/index.js
+++ Lynk/backend/index.js
@@ -1,24 +1,9 @@
-import { createClient } from "@supabase/supabase-js"
-import { config } from "dotenv";
 
-config();
-
-const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseKey = process.env.SUPABASE_SECRET_KEY;
-
-const supabase = createClient(supabaseUrl, supabaseKey);
+export * from './supabase/auth.js';
+export * from './supabase/contacts.js';
+export * from './supabase/users.js';
+export * from './supabase/storage.js';
 
 
-// typeahead search
-export async function fetchInitialContactsForSearch(firstChar) {
-  const { data, error } = await supabase
-    .from('connections')
-    .select("*")
-    .ilike('name', `${firstChar}%`);
-  if (error) {
-    console.error('Error fetching data:', error);
-    return [];
-  }
-  return data;
-}
+export { Trie } from './trie.js';
 
